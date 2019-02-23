4931a46bd82336e41373d558ff53a221d2004035
richardluong127 <richardluong127@gmail.com>
2019-02-23 13:10:47 -0800

    testing jawsdb

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 const Sequelize = require("sequelize");
 
-module.exports =  new Sequelize("piggybusiness", process.env.user, process.env.pw, {
+module.exports =  if (process.env.JAWSDB_URL) { new Sequelize (
+  process.env.JAWSDB_URL, "wv6jalv1r952yogf", "w4jcn01p5aam3s4t"
+) } else
+  
+  new Sequelize("piggybusiness", process.env.user, process.env.pw, {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
